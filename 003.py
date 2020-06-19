76305529b2b5abb776544773adca2ac34124ed20
Wahyu Wibowo <wahyuoxs135@gmail.com>
2020-06-19 09:20:16 +0700

    menambahkan no3

--- 003.py
+++ 003.py
@@ -1,16 +1,25 @@
 import requests, json
 
+def combinePostAndUsers(res1, res2):
+    new_data = []
+    for data in  res1:
+        if 'users' in res1:
+            pass
+        else:
+            for x in res2:
+                if data['userId'] == x['id']:
+                    data.update({'user' : x})
+        new_data.append(data)
+    
+    toJson =  json.dumps(new_data, indent=4)
+    fwrite = open('file.json', 'w')
+    fwrite.write(toJson)
+
+
 get1 = requests.get("https://jsonplaceholder.typicode.com/posts") 
 res1 = get1.json()
 
 get2 = requests.get("https://jsonplaceholder.typicode.com/users") 
-data = get2.json()
-print(data)
+res2 = get2.json()
 
-# merge = requests.post("https://jsonplaceholder.typicode.com/posts", params=data)
-
-# toJson = json.dumps(merge, indent=4)
-# fwrite = open("file.json", 'w')
-# fwrite.write(toJson)
-
-
+json = combinePostAndUsers(res1, res2)
