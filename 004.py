45d5ab4185ae820c452e3b7f36df7c830de6f7d7
Wahyu Wibowo <wahyuoxs135@gmail.com>
2020-06-19 16:54:19 +0700

    revisi no 4

--- 004.py
+++ 004.py
@@ -2,60 +2,41 @@ import requests
 
 class Movie:
     def get_indonesian_movies(self):
-        url = requests.get("https://api.themoviedb.org/3/discover/movie?api_key=6ee5671a60dffcfba9b370c145061ef8&language=id-ID&region=ID&sort_by=popularity.asc&include_adult=false&include_video=false&page=1")
+        url = requests.get(" https://api.themoviedb.org/3/discover/movie?api_key=6ee5671a60dffcfba9b370c145061ef8&language=id-ID&region=ID&sort_by=popularity.asc&include_adult=false&include_video=false&page=1")
         respon = url.json()
-        print(respon)
+        result = list(map(lambda a: a['title'], respon['results']))
+        print(result[0:10])
 
     def get_movie_by_keanureeves(self):
         url = requests.get("https://api.themoviedb.org/3/person/6384/movie_credits?api_key=6ee5671a60dffcfba9b370c145061ef8&language=en-US")
         respon = url.json()
-        print(respon)
+        result = list(map(lambda a: a['title'], respon['cast']))
+        print(result)
 
-    # def get_movie_by_RobertDowney(self):
-    #     url = requests.get("https://api.themoviedb.org/3/person/3223/movie_credits?api_key=6ee5671a60dffcfba9b370c145061ef8&language=en-US")
-    #     respon = url.json()
-    #     # print(respon)
-    #     return respon
-
-    # def get_movie_by_TomHolland(self):
-    #     url = requests.get(" https://api.themoviedb.org/3/person/1136406/movie_credits?api_key=6ee5671a60dffcfba9b370c145061ef8&language=en-US")
-    #     respon = url.json()
-    #     # print(respon)
-    #     return respon
-
-    def tiga(self):
+    def get_movie_played_by_RobertDowneyJr_and_TomHolland(self):
         respon1 = requests.get("https://api.themoviedb.org/3/person/3223/movie_credits?api_key=6ee5671a60dffcfba9b370c145061ef8&language=en-US")
-        data1 = respon1.json()['cast']
+        fetch1 = respon1.json()
+        data1 = list(map(lambda a: a['title'], fetch1['cast']))
         respon2 = requests.get(" https://api.themoviedb.org/3/person/1136406/movie_credits?api_key=6ee5671a60dffcfba9b370c145061ef8&language=en-US")
-        data2 = respon2.json()['cast']
+        fetch2 = respon2.json()
+        data2 = list(map(lambda a: a['title'], fetch2['cast']))
         new_data = []
         for i in data1:
             for b in data2:
                 if i == b:
-                    new_data.append()
+                    new_data.append(b)
         print(new_data)
 
     def get_popular_movie_2016(self):
-        url = requests.get("https://api.themoviedb.org/3/discover/movie?api_key=6ee5671a60dffcfba9b370c145061ef8&language=en-US&sort_by=popularity.asc&include_adult=false&include_video=false&page=1&year=2016&vote_count.gte=7.5")
+        url = requests.get("https://api.themoviedb.org/3/discover/movie?api_key=6ee5671a60dffcfba9b370c145061ef8&language=en-US&sort_by=popularity.asc&include_adult=false&include_video=false&page=1&primary_release_year=2016&vote_count.gte=7.5")
         respon = url.json()
-        print(respon)
+        result = list(map(lambda a: a['title'], respon['results']))
+        print(result)
 
 
 
 movie = Movie()
-# movie.get_indonesian_movies()
-# movie.get_popular_movie_2016()
-# movie.get_movie_by_keanureeves()
-movie.tiga()
-
-# res1 = movie.get_movie_by_RobertDowney()
-# res2 = movie.get_movie_by_TomHolland()
-
-# def combine(res1, res2):
-#     new_data = []
-#     for a in res1:
-#         for b in res2:
-#             if a == b:
-#                 new_data.append(a)
-#     print(list(new_data))
-# combine(res1, res2)
+movie.get_indonesian_movies()
+movie.get_popular_movie_2016()
+movie.get_movie_by_keanureeves()
+movie.get_movie_played_by_RobertDowneyJr_and_TomHolland()
